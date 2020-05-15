089a59476e666f8f7cf1906496c3600e4fb647cc
sogo <sogo@SogonoMacBook-Pro2.local>
2020-05-15 09:25:09 +0900

    comicsのルートをcomics/allに変更

--- routes/api.php
+++ routes/api.php
@@ -28,8 +28,8 @@ Route::get('/unauthorized', function() {
 Route::get('/users', 'UsersController@index')->name('api.users.index');
 Route::get('/users/{id}', 'UsersController@show')->name('api.users.show');
 
-Route::get('/comics', 'ComicsController@index')->name('api.comics.index');
-Route::get('/comics/{id}', 'ComicsController@show')->name('api.comics.show');
+Route::get('/comics/all', 'ComicsController@index')->name('api.comics.index');
+Route::get('/comics/all/{id}', 'ComicsController@show')->name('api.comics.show');
 
 
 // ログイン状態
